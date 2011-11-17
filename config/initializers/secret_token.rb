8981c6fcae7cd8ff7ea7beb84407e44a50806e07
Neil Edwards <neil@plasticwater.com>
2011-11-17 07:37:22 +0000

    moved to rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotoPool::Application.config.secret_token = '097bc03e3e7966f707fb8a691e5d599e765e6cebf2ecaf8e7968acc70bbd114922bb161b9f33228ff1a0e58fb3751e46c2afd6bea6f23e42994c77c9c47a75b5'
